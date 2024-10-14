990537a3a57a5f7e011b64c8691dca02afe47da0
TAHAN <wwwkl545@gmail.com>
2024-10-14 22:05:38 +0300

    create Loading component

---
+++ src/components/UI/Loading.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="absolute w-full h-full dark:bg-black bg-white dark:bg-opacity-20 bg-opacity-20 place-items-center grid z-50">
+            <span class="animate-spin   w-10 h-10 rounded-full border-4 border-gray-500 border-l-transparent"></span>
+    </div>
+</template>
