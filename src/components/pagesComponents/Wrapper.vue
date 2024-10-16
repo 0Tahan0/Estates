a700b08c097895333ea4e86790672f793a8a612d
TAHAN <wwwkl545@gmail.com>
2024-10-16 23:47:19 +0300

    change place

---
+++ src/components/pagesComponents/Wrapper.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="dark:text-white dark:bg-dark bg-light">
+    <slot></slot>
+  </div>
+</template>
---
+++ src/components/pagesComponents/WrapperEl.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="dark:bg-darkEl bg-lightEl dark:text-white text-dark">
+        <slot></slot>
+    </div>
+</template>
